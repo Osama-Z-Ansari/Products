faf19d05dfb4cce5b40ee2f625a8c8f8130ab854
Osama-Z-Ansari <73730429+Osama-Z-Ansari@users.noreply.github.com>
2021-10-09 02:08:00 -0400

    Add project files.

---
+++ Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+
+    }
+}
---
+++ Project/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Display Order")]
+        [Range(1,int.MaxValue,ErrorMessage ="Display Order for Category must be greater then 0")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
